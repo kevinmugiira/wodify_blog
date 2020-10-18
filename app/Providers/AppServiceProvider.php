cbfe190f474618defbbf5710fffabeb6310f691f
kevinmugiira <kevinm5740@aol.com>
2020-10-18 10:46:16 +0300

    added to AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,7 +13,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        //registering for heroku database deployment
+        Schema::defaultStringLength(191);
     }
 
     /**
